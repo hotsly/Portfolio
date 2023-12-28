088de8288da74d90bd4538d8f242ac7d2239984e
hotsly <154548301+hotsly@users.noreply.github.com>
2023-12-28 14:04:55 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container mx-auto'>
+      <input className='rounded-full border-solid border p-2 flex-initial w-96 border-zinc-400 bg-transparent text-white' placeholder='Search' type="search" id="i-inf" />
+    </div>
+  )
+}
+
+export default App
