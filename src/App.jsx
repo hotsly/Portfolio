5937ebd9a662a1a5cb139f671e9e67b61354fc99
hotsly <154548301+hotsly@users.noreply.github.com>
2023-12-28 16:45:33 -0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+  const title = ['B', 'i', 'e', 'n']
+  const colors = ['text-red-500', 'text-yellow-300', 'text-green-500', 'text-blue-500']
+  const out = title.map((letters, index) => {
+    return <span key={letters} className={colors[index]}>{letters}</span>
+  })
+
 const App = () => {
   return (
-    <div className='container mx-auto'>
-      <input className='rounded-full border-solid border p-2 flex-initial w-96 border-zinc-400 bg-transparent text-white' placeholder='Search' type="search" id="i-inf" />
+    <div className='container mx-auto h-screen flex items-center justify-center'>
+      <h1 className='text-white font-plus-jakarta-sans text-6xl'>{out}</h1>
+      <input className={`rounded-full border-solid border p-2 flex-initial w-96 border-zinc-400 bg-transparent {}`} placeholder='' type="search" id="i-inf" />
     </div>
   )
 }
